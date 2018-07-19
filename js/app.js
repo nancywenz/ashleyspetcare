7f9359896796c44e5a9f7328eae217bff8abbe48
Wenz <nancy.wenz@oldham.kyschools.us>
2018-07-19 10:09:04 -0400

    try again

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//pic generator
+var images = [
+  
+
+
+]
+
+
+function newPic() {
+  var randomNumber = Math.floor(Math.random() * (images.length));
+  document.getElementById('picDisplay').innerHTML = images[randomNumber];
+}
+
+// jss for contact form
+
+$("#submit").click(function() {
+    submit();
+  });
+});
+
+function submit() {
+  alert("Ashley will contact you soon!");
+}
